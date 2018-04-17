748d4580472ef98a186f34e207ed282e00448e1b
willmonahan <willmonahan13@gmail.com>
2018-04-17 18:12:35 -0400

    added php includes to the homepage

--- index.php
+++ index.php
@@ -7,18 +7,14 @@
 		<title>Will Monahan</title>
 
 		<link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.1.0/css/bootstrap.min.css" integrity="sha384-9gVQ4dYFwwWSjIDZnLEWnxCjeSWFphJiwGPXr1jddIhOegiu1FwO5qRGvFXOdJZ4" crossorigin="anonymous">
-		<link href="css/main.css" rel="stylesheet">
+		<link href="./css/main.css" rel="stylesheet">
 		<script defer src="https://use.fontawesome.com/releases/v5.0.10/js/all.js" integrity="sha384-slN8GvtUJGnv6ca26v8EzVaR9DC58QEwsIk9q1QXdCU8Yu8ck/tL/5szYlBbqmS+" crossorigin="anonymous"></script>
-		<script
-  			src="https://code.jquery.com/jquery-3.3.1.min.js"
-  			integrity="sha256-FgpCb/KJQlLNfOu91ta32o/NMZxltwRo8QtmkMRdAu8="
-  			crossorigin="anonymous"></script>
 	</head>
 	<body class="d-flex flex-column justify-content-between">
 
 		<div class="container">
 
-			<!--include navbar-->
+			<?php include "./resources/navbar.html" ?>
 
 			<div class="row">
 
@@ -39,10 +35,11 @@
 			
 		</div><!--Container End-->
 
-		<!--include footer-->
+		<?php include "./resources/footer.html" ?>
 
 		<!-- Optional JavaScript -->
-		<!-- Popper.js, then Bootstrap JS -->
+		<!-- jQuery first, then Popper.js, then Bootstrap JS -->
+		<script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
 		<script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.0/umd/popper.min.js" integrity="sha384-cs/chFZiN24E4KMATLdqdvsezGxaGsi4hLGOzlXwp5UZB1LY//20VyM2taTB4QvJ" crossorigin="anonymous"></script>
 		<script src="https://stackpath.bootstrapcdn.com/bootstrap/4.1.0/js/bootstrap.min.js" integrity="sha384-uefMccjFJAIv6A+rW+L4AHf99KvxDjWSu1z9VI8SKNVmz4sk7buKt/6v9KI65qnm" crossorigin="anonymous"></script>
 		<script type="text/javascript">
