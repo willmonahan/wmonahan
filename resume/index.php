a129fae62fa25b50a5ef0511f6683a45aa94849d
willmonahan <willmonahan13@gmail.com>
2018-04-21 19:17:38 -0400

    add page for resume
    right now it uses Ilya's modal
    I'm going to use Ilya's template, change it from bootstrap modal to bootstrap card

---
+++ resume/index.php
@@ -0,0 +1,174 @@
+<!DOCTYPE html>
+<html lang="en">
+	<head>
+		<!-- Required meta tags -->
+		<meta charset="utf-8">
+		<meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+		<title>Will Monahan</title>
+		<link rel="shortcut icon" href="/images/favicon.png">
+
+
+		<link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.1.0/css/bootstrap.min.css" integrity="sha384-9gVQ4dYFwwWSjIDZnLEWnxCjeSWFphJiwGPXr1jddIhOegiu1FwO5qRGvFXOdJZ4" crossorigin="anonymous">
+		<link href="/css/main.css" rel="stylesheet">
+		<script defer src="https://use.fontawesome.com/releases/v5.0.10/js/all.js" integrity="sha384-slN8GvtUJGnv6ca26v8EzVaR9DC58QEwsIk9q1QXdCU8Yu8ck/tL/5szYlBbqmS+" crossorigin="anonymous"></script>
+	</head>
+	<body class="d-flex flex-column justify-content-between">
+
+		<div class="container">
+
+			<?php include $_SERVER['DOCUMENT_ROOT']."/resources/navbar.html" ?>
+
+			<div class="row">
+
+				<div class="modal-dialog modal-dialog-centered modal-lg" role="document">
+					<div class="modal-content">
+						<div class="modal-header">
+							<a class="btn btn-outline-info" download href="assets/pdf/ilya-resume-jan2018.pdf"><i class="fas fa-file-pdf"></i> download .pdf</a>
+							<button class="btn btn-outline-danger" data-dismiss="modal" aria-label="Close"><i class="fas fa-times"></i> close</button>
+						</div>
+						<div class="modal-body p-5">
+							<div class="row">
+								<div class="col-lg-7">
+									<div class="row px-3">
+										<div class="col-12">
+											<h1 class="row font-weight-bold text-dark">Ilya Motamedi</h1>
+											<h4 class="row font-weight-bold pt-3 text-resume-primary">EXPERIENCE</h4>
+										</div>
+									</div>
+									<div class="row px-3 mb-3">
+										<div class="col-12">
+											<h6 class="row font-weight-bold mb-0 text-dark">Nspire Innovation Network | Toronto</h6>
+											<i class="row text-dark">Chair, National Business & Technology Conference</i>
+											<small class="row">SEPTEMBER 2017 - PRESENT (9M+)</small>
+											<small class="row">Overseeing the development of a 250+ delegate conference with an 18-year legacy, dedicated to the intersection of business and technology. Collaboration with some of Canada's biggest brands: MBNA, Rogers, PwC, and more. Nspire is a registered Canadian nonprofit.</small>
+										</div>
+									</div>
+									<div class="row px-3 mb-3">
+										<div class="col-12">
+											<h6 class="row font-weight-bold mb-0 text-dark">Top Uniforms | Toronto</h6>
+											<i class="row text-dark">Full Stack Developer</i>
+											<small class="row">OCTOBER 2016 - AUGUST 2017 (10M)</small>
+											<small class="row">Design, development, deployment, and support of a MEAN stack application enabling highly-automated graphics, quote-generation, and inventory-tracking workflows (as much as 12x time savings over manual processes) enabling staff, graphic designers, and salespeople to better meet the needs of clients.</small>
+										</div>
+									</div>
+									<div class="row px-3 mb-3">
+										<div class="col-12">
+											<h6 class="row font-weight-bold mb-0 text-dark">Nspire Innovation Network | Toronto</h6>
+											<i class="row text-dark">Operations and Marketing Lead, Toronto Chapter</i>
+											<small class="row">MAY 2016 - AUGUST 2017 (1Y 3M)</small>
+											<small class="row">Logistics management for focused events geared toward students with an interest in financial and health technology. Development of marketing plans and collection of experience feedback from over 300 delegates to enable more effective marketing strategies for future events.</small>
+										</div>
+									</div>
+									<div class="row px-3 mb-3">
+										<div class="col-12">
+											<h6 class="row font-weight-bold mb-0 text-dark">Voxme Software, Inc. | Toronto</h6>
+											<i class="row text-dark">Intern</i>
+											<small class="row">SUMMER 2014 (1M)</small>
+											<small class="row">Translation of technical manuals and documents from English to French. Ensuring design consistency and linguistic integrity across document versions and languages.</small>
+										</div>
+									</div>
+									<div class="row px-3">
+										<div class="col-12">
+											<h6 class="row font-weight-bold mb-0 text-dark">Snyder Architects | Toronto</h6>
+											<i class="row text-dark">Intern</i>
+											<small class="row">SUMMER 2013 (1M)</small>
+											<small class="row">Collection and compilation of data from on-site surveys. Cold-calling and office administration.</small>
+										</div>
+									</div>                                        
+								</div>
+								<div class="col-lg-5 mb-3">
+									<div class="row px-3">
+										<div class="col-12 text-right">
+											<h1 class="row font-weight-bold text-white invisible hidden-lg">__</h1>
+											<h4 class="row text-left font-weight-bold pt-3 text-resume-primary">SKILLS</h4>
+										</div>
+									</div>
+									<div class="row px-3 mb-3">
+										<div class="col-12">
+											<small class="row font-weight-bold text-dark">Software Development</small>
+											<small class="row">Python (TensorFlow, Flask, Django)</small>
+											<small class="row">JavaScript (Node.js, Angular, React, jQuery)</small>
+											<small class="row">HTML5/CSS3 (Bootstrap, Sass, Jekyll)</small>
+											<small class="row">Google Apps Script (Google docs automation)</small>
+											<small class="row">Visual Basic (Windows scripting/automation)</small>
+											<small class="row">C# (.NET)</small>
+											<small class="row">Java</small>
+											<small class="row">SQL/MongoDB</small>
+											<small class="row">Command Line/Terminal scripting</small>
+											<small class="row">Version Control (Git/Github)</small>
+											<small class="row">Rapid prototyping</small>
+										</div>
+									</div>
+									<div class="row px-3 mb-3">
+										<div class="col-12">
+											<small class="row font-weight-bold text-dark">General Technical Skills</small>
+											<small class="row">Visual presentations (Powerpoint/GSlides)</small>
+											<small class="row">Data analysis/manipulation (Excel/GSheets)</small>
+											<small class="row">Adobe CC/CS</small>
+											<small class="row">Multi-OS superuser (Windows/macOS/Linux)</small>
+											<small class="row">LaTeX typesetting</small>                                                
+										</div>
+									</div>
+									<div class="row px-3 mb-3">
+										<div class="col-12">
+											<small class="row font-weight-bold text-dark">Soft Skills</small>
+											<small class="row">Self-motivated, organized, and responsible</small>
+											<small class="row">Strong leader and effective project manager</small>
+											<small class="row">Excellent communicator and team player</small>
+											<small class="row">Fun, outgoing, and approachable</small>
+										</div>
+									</div>
+									<div class="row px-3 mb-3">
+										<div class="col-12">
+											<small class="row font-weight-bold text-dark">Languages</small>
+											<small class="row">English - Fluent</small>
+											<small class="row">French - Fluent</small>
+											<small class="row">Farsi (Persian) - Conversational</small>
+											<small class="row">Spanish - Beginner</small>
+										</div>
+									</div>
+									<div class="row px-3 mb-3">
+										<div class="col-12">
+											<h4 class="row font-weight-bold text-resume-primary">QUALIFICATIONS</h4>
+											<small class="row font-weight-bold text-dark">CompTIA A+ IT certification</small>
+											<small class="row font-weight-bold text-dark">CPR-A/-B/-C certification</small>
+										</div>
+									</div>
+									<div class="row px-3">
+										<div class="col-12">
+											<h4 class="row font-weight-bold text-resume-primary">EDUCATION</h4>
+											<h6 class="row font-weight-bold mb-0 text-dark">University of Toronto</h6>
+											<i class="row text-dark">Hons.B.Sc. Computer Science</i>
+											<small class="row mb-3">SEPTEMBER 2014 - PRESENT</small>
+											<h6 class="row font-weight-bold mb-0 text-dark">University of Toronto Schools</h6>
+											<i class="row text-dark">Secondary School</i>
+											<small class="row">SEPTEMBER 2008 - JUNE 2014</small>
+										</div>
+									</div>
+								</div>
+							</div>
+						</div>
+						<div class="modal-footer">
+							<small class="text-muted">&copy; Ilya Motamedi 2018.</small>
+						</div>
+					</div>
+				</div>
+
+			</div>
+			
+		</div><!--Container End-->
+
+		<?php include $_SERVER['DOCUMENT_ROOT']."/resources/footer.html" ?>
+
+		<!-- Optional JavaScript -->
+		<!-- jQuery first, then Popper.js, then Bootstrap JS -->
+		<script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
+		<script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.0/umd/popper.min.js" integrity="sha384-cs/chFZiN24E4KMATLdqdvsezGxaGsi4hLGOzlXwp5UZB1LY//20VyM2taTB4QvJ" crossorigin="anonymous"></script>
+		<script src="https://stackpath.bootstrapcdn.com/bootstrap/4.1.0/js/bootstrap.min.js" integrity="sha384-uefMccjFJAIv6A+rW+L4AHf99KvxDjWSu1z9VI8SKNVmz4sk7buKt/6v9KI65qnm" crossorigin="anonymous"></script>
+		<script type="text/javascript">
+			$(document).ready(function() {
+				$("#home").addClass("active");
+			});
+		</script>
+	</body>
+</html>
