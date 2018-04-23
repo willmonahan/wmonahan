e7186370c278d7f922c3ab720d76c93855477c4c
willmonahan <willmonahan13@gmail.com>
2018-04-23 11:44:09 -0400

    change resume data to match google doc
    still need to add NYU majors/minors/GPA data
    still need to do more tweaking to visual style and layout
    it's still in a modal, but modal is embedded in body
    may change to card later down the line

--- resume/index.php
+++ resume/index.php
@@ -9,6 +9,7 @@
 
 
 		<link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.1.0/css/bootstrap.min.css" integrity="sha384-9gVQ4dYFwwWSjIDZnLEWnxCjeSWFphJiwGPXr1jddIhOegiu1FwO5qRGvFXOdJZ4" crossorigin="anonymous">
+		<link href="https://fonts.googleapis.com/css?family=Noto+Serif:400,700" rel="stylesheet">
 		<link href="/css/main.css" rel="stylesheet">
 		<script defer src="https://use.fontawesome.com/releases/v5.0.10/js/all.js" integrity="sha384-slN8GvtUJGnv6ca26v8EzVaR9DC58QEwsIk9q1QXdCU8Yu8ck/tL/5szYlBbqmS+" crossorigin="anonymous"></script>
 	</head>
@@ -20,7 +21,7 @@
 
 			<div class="row">
 
-				<div class="modal-dialog modal-dialog-centered modal-lg" role="document">
+				<div class="modal-dialog modal-dialog-centered modal-lg resume" role="document">
 					<div class="modal-content">
 						<div class="modal-header">
 							<a class="btn btn-outline-info" download href="assets/pdf/ilya-resume-jan2018.pdf"><i class="fas fa-file-pdf"></i> download .pdf</a>
@@ -31,48 +32,48 @@
 								<div class="col-lg-7">
 									<div class="row px-3">
 										<div class="col-12">
-											<h1 class="row font-weight-bold text-dark">Ilya Motamedi</h1>
+											<h1 class="row font-weight-bold text-dark">Will Monahan</h1>
 											<h4 class="row font-weight-bold pt-3 text-resume-primary">EXPERIENCE</h4>
 										</div>
 									</div>
 									<div class="row px-3 mb-3">
 										<div class="col-12">
-											<h6 class="row font-weight-bold mb-0 text-dark">Nspire Innovation Network | Toronto</h6>
-											<i class="row text-dark">Chair, National Business & Technology Conference</i>
-											<small class="row">SEPTEMBER 2017 - PRESENT (9M+)</small>
-											<small class="row">Overseeing the development of a 250+ delegate conference with an 18-year legacy, dedicated to the intersection of business and technology. Collaboration with some of Canada's biggest brands: MBNA, Rogers, PwC, and more. Nspire is a registered Canadian nonprofit.</small>
+											<h6 class="row font-weight-bold mb-0 text-dark">NYU Courant Institute</h6>
+											<h6 class="row text-dark mb-0">Tutor & Grader</h6>
+											<small class="row resume-date">JAN 2018 - PRESENT</small>
+											<small class="row">Tutoring, grading, and misc. TA duties for Interactive Computing and Intro to Web Design courses. Teaching concepts related to various web frameworks and game design for web, AR, and VR.</small>
 										</div>
 									</div>
 									<div class="row px-3 mb-3">
 										<div class="col-12">
-											<h6 class="row font-weight-bold mb-0 text-dark">Top Uniforms | Toronto</h6>
-											<i class="row text-dark">Full Stack Developer</i>
-											<small class="row">OCTOBER 2016 - AUGUST 2017 (10M)</small>
-											<small class="row">Design, development, deployment, and support of a MEAN stack application enabling highly-automated graphics, quote-generation, and inventory-tracking workflows (as much as 12x time savings over manual processes) enabling staff, graphic designers, and salespeople to better meet the needs of clients.</small>
+											<h6 class="row font-weight-bold mb-0 text-dark">Toronto International Film Festival</h6>
+											<h6 class="row text-dark mb-0">Invitations Assistant</h6>
+											<small class="row resume-date">JUN 2016 - AUG 2016 & JUN 2017 - AUG 2017</small>
+											<small class="row">Administrative responsibilities including invitation of festival films & guests, correspondence with stakeholders, and record maintenance of festival SQL database.</small>
 										</div>
 									</div>
 									<div class="row px-3 mb-3">
 										<div class="col-12">
-											<h6 class="row font-weight-bold mb-0 text-dark">Nspire Innovation Network | Toronto</h6>
-											<i class="row text-dark">Operations and Marketing Lead, Toronto Chapter</i>
-											<small class="row">MAY 2016 - AUGUST 2017 (1Y 3M)</small>
-											<small class="row">Logistics management for focused events geared toward students with an interest in financial and health technology. Development of marketing plans and collection of experience feedback from over 300 delegates to enable more effective marketing strategies for future events.</small>
+											<h6 class="row font-weight-bold mb-0 text-dark">British Academy of Film & TV Arts</h6>
+											<h6 class="row text-dark mb-0">Intern</h6>
+											<small class="row resume-date">SEP 2016 - DEC 2016</small>
+											<small class="row">Streamlined event-planning workflow for BAFTA members screening committee, coordinated to liase with members and manage online media & content. Created automated workflow to integrate Google Sheets with committee’s existing event-planning tools and systems.</small>
 										</div>
 									</div>
 									<div class="row px-3 mb-3">
 										<div class="col-12">
-											<h6 class="row font-weight-bold mb-0 text-dark">Voxme Software, Inc. | Toronto</h6>
-											<i class="row text-dark">Intern</i>
-											<small class="row">SUMMER 2014 (1M)</small>
-											<small class="row">Translation of technical manuals and documents from English to French. Ensuring design consistency and linguistic integrity across document versions and languages.</small>
+											<h6 class="row font-weight-bold mb-0 text-dark">Partners Film</h6>
+											<h6 class="row text-dark mb-0">Office & Production Assistant</h6>
+											<small class="row resume-date">MAY 2015 - AUG 2015</small>
+											<small class="row">Administrative responsibilities including invitation of festival films & guests, correspondence with stakeholders, and record maintenance of festival SQL database.</small>
 										</div>
 									</div>
 									<div class="row px-3">
 										<div class="col-12">
-											<h6 class="row font-weight-bold mb-0 text-dark">Snyder Architects | Toronto</h6>
-											<i class="row text-dark">Intern</i>
-											<small class="row">SUMMER 2013 (1M)</small>
-											<small class="row">Collection and compilation of data from on-site surveys. Cold-calling and office administration.</small>
+											<h6 class="row font-weight-bold mb-0 text-dark">Toronto International Film Festival</h6>
+											<h6 class="row text-dark mb-0">Next Wave Committee Member</h6>
+											<small class="row resume-date">JUN 2012 - JUN 2014</small>
+											<small class="row">Coordination of programming, branding, marketing and outreach for the TIFF Next Wave Youth Film Festival.</small>
 										</div>
 									</div>                                        
 								</div>
@@ -85,71 +86,43 @@
 									</div>
 									<div class="row px-3 mb-3">
 										<div class="col-12">
-											<small class="row font-weight-bold text-dark">Software Development</small>
-											<small class="row">Python (TensorFlow, Flask, Django)</small>
-											<small class="row">JavaScript (Node.js, Angular, React, jQuery)</small>
-											<small class="row">HTML5/CSS3 (Bootstrap, Sass, Jekyll)</small>
-											<small class="row">Google Apps Script (Google docs automation)</small>
-											<small class="row">Visual Basic (Windows scripting/automation)</small>
-											<small class="row">C# (.NET)</small>
+											<small class="row font-weight-bold text-dark skill-header">Software Development</small>
+											<small class="row">Python (TensorFlow, MatPlotLib, etc)</small>
+											<small class="row">Javascript (JQuery, AJAX, p5, D3)</small>
+											<small class="row">Database Design (SQL, MongoDB)</small>
+											<small class="row">HTML5 / CSS3 (Bootstrap, Jekyll)</small>
 											<small class="row">Java</small>
-											<small class="row">SQL/MongoDB</small>
-											<small class="row">Command Line/Terminal scripting</small>
+											<small class="row">PHP</small>
 											<small class="row">Version Control (Git/Github)</small>
+											<small class="row">Command Line / Terminal scripting</small>
 											<small class="row">Rapid prototyping</small>
 										</div>
 									</div>
 									<div class="row px-3 mb-3">
 										<div class="col-12">
-											<small class="row font-weight-bold text-dark">General Technical Skills</small>
-											<small class="row">Visual presentations (Powerpoint/GSlides)</small>
-											<small class="row">Data analysis/manipulation (Excel/GSheets)</small>
-											<small class="row">Adobe CC/CS</small>
-											<small class="row">Multi-OS superuser (Windows/macOS/Linux)</small>
-											<small class="row">LaTeX typesetting</small>                                                
-										</div>
-									</div>
-									<div class="row px-3 mb-3">
-										<div class="col-12">
-											<small class="row font-weight-bold text-dark">Soft Skills</small>
-											<small class="row">Self-motivated, organized, and responsible</small>
-											<small class="row">Strong leader and effective project manager</small>
-											<small class="row">Excellent communicator and team player</small>
-											<small class="row">Fun, outgoing, and approachable</small>
-										</div>
-									</div>
-									<div class="row px-3 mb-3">
-										<div class="col-12">
-											<small class="row font-weight-bold text-dark">Languages</small>
-											<small class="row">English - Fluent</small>
-											<small class="row">French - Fluent</small>
-											<small class="row">Farsi (Persian) - Conversational</small>
-											<small class="row">Spanish - Beginner</small>
-										</div>
-									</div>
-									<div class="row px-3 mb-3">
-										<div class="col-12">
-											<h4 class="row font-weight-bold text-resume-primary">QUALIFICATIONS</h4>
-											<small class="row font-weight-bold text-dark">CompTIA A+ IT certification</small>
-											<small class="row font-weight-bold text-dark">CPR-A/-B/-C certification</small>
+											<small class="row font-weight-bold text-dark skill-header">General Technical Skills</small>
+											<small class="row">GPresentations (Powerpoint / GSlides)</small>
+											<small class="row">Data analysis (Excel / GSheets)</small>
 										</div>
 									</div>
 									<div class="row px-3">
 										<div class="col-12">
 											<h4 class="row font-weight-bold text-resume-primary">EDUCATION</h4>
-											<h6 class="row font-weight-bold mb-0 text-dark">University of Toronto</h6>
-											<i class="row text-dark">Hons.B.Sc. Computer Science</i>
-											<small class="row mb-3">SEPTEMBER 2014 - PRESENT</small>
+
+											<h6 class="row font-weight-bold mb-0 text-dark">New York University</h6>
+											<h6 class="row text-dark mb-0">Bachelor's Degree</h6>
+											<small class="row mb-3 resume-date">GRADUATING MAY 2019</small>
+
 											<h6 class="row font-weight-bold mb-0 text-dark">University of Toronto Schools</h6>
-											<i class="row text-dark">Secondary School</i>
-											<small class="row">SEPTEMBER 2008 - JUNE 2014</small>
+											<h6 class="row text-dark mb-0">Secondary School</h6>
+											<small class="row resume-date">GRADUATED JUNE 2014</small>
 										</div>
 									</div>
 								</div>
 							</div>
 						</div>
 						<div class="modal-footer">
-							<small class="text-muted">&copy; Ilya Motamedi 2018.</small>
+							<small class="text-muted">&copy; Will Monahan 2018</small>
 						</div>
 					</div>
 				</div>
